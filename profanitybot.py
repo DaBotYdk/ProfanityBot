083d6597400f2ec9aca627f0afd8d16610794689
DaBotYdk <44483476+DaBotYdk@users.noreply.github.com>
2018-10-28 14:17:04 -0700

    Add files via upload

---
+++ profanitybot.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import random
+
+swears = ['fuck','shit','bitch','dick','ass','tit']
+
+pfnt = ["Watch your profanity, ","Don't swear on my Christian Discord server, "]
+
+Client = discord.Client()
+
+client = commands.Bot(command_prefix = "$")
+
+
+@client.event
+async def on_ready():
+    print("Bot is ready!")
+
+@client.event
+async def on_message(message):
+    for swr in swears:
+        if swr in message.content.lower():
+            await client.send_message(message.channel, (random.choice(pfnt) + str(message.author) + '!'))
+
+client.run("NTA0ODM3NDM1MDEyOTM5Nzc2.DraJGg.GL52r8mFz6YF88t5mbbqG8v40gw")
