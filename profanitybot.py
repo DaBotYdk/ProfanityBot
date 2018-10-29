2e658831b6be27653d774e591ae6edd75ca1390a
DaBotYdk <44483476+DaBotYdk@users.noreply.github.com>
2018-10-28 19:47:37 -0700

    Update profanitybot.py

--- profanitybot.py
+++ profanitybot.py
@@ -22,7 +22,7 @@ async def on_ready():
 async def on_message(message):
     for swr in swears:
         if swr in message.content.lower():
-            await bot.add_reaction(message, 'scream')
+            await bot.add_reaction(message, scream)
             await client.send_message(message.channel, (random.choice(pfnt) + str(message.author) + '!'))
 
 client.run(os.getenv("TOKEN"))
